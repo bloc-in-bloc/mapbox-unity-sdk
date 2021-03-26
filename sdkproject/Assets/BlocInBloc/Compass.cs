ad53b9b54f8f18866953f94c545a5f893591f927
blocovin <kevin@blocinbloc.com>
2021-03-26 11:34:06 +0100

    Update to Unity 2020.3.20f1 & Use Arfoundation

---
+++ sdkproject/Assets/BlocInBloc/Compass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Mapbox.Examples;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour {
+    public Text magneticNorth;
+    public Text trueNorth;
+    public Text interference;
+    public Text distance;
+
+    public Image magneticNorthImg;
+    public Image trueNorthImg;
+    public CustomSpawnOnMap customSpawn;
+
+    void Update () {
+        magneticNorth.text = $"Magnetic North: {Input.compass.magneticHeading}";
+        trueNorth.text = $"True North: {Input.compass.trueHeading}";
+        interference.text = Input.compass.rawVector.magnitude > 60 ? $"Interference: <color=red>{ Input.compass.rawVector.magnitude }</color>" : $"Interference: <color=green>{ Input.compass.rawVector.magnitude }</color>";
+
+        // read the magnetometer / compass value and generate a quaternion
+        Quaternion trueHeading = Quaternion.Euler (0, 0, Input.compass.trueHeading);
+        // interpolate between old and new position
+        trueNorthImg.transform.localRotation = Quaternion.Slerp (trueNorthImg.transform.localRotation, trueHeading, 0.05f);
+
+        // read the magnetometer / compass value and generate a quaternion
+        Quaternion magneticHeading = Quaternion.Euler (0, 0, Input.compass.magneticHeading);
+        // interpolate between old and new position
+        magneticNorthImg.transform.localRotation = Quaternion.Slerp (trueNorthImg.transform.localRotation, magneticHeading, 0.05f);
+
+        if (customSpawn._isSpawn) {
+            distance.text = $"Distance to spawn: {Convert.ToString (Vector3.Distance (Camera.main.transform.position, customSpawn._spawnedObject.transform.position))}";
+        }
+    }
+}
---
+++ sdkproject/Assets/BlocInBloc/CustomSpawnOnMap.cs
@@ -0,0 +1,46 @@
+﻿using Mapbox.Unity.Ar;
+
+namespace Mapbox.Examples {
+    using System.Collections.Generic;
+    using Mapbox.Unity.Map;
+    using Mapbox.Unity.MeshGeneration.Factories;
+    using Mapbox.Unity.Utilities;
+    using Mapbox.Utils;
+    using UnityEngine;
+
+    public class CustomSpawnOnMap : MonoBehaviour {
+        [SerializeField]
+        AbstractMap _map;
+        Vector2d _location;
+
+        public GameObject _spawnedObject;
+        public AverageHeadingAlignmentStrategy averageHeadingAlignmentStrategy;
+        public bool _isSpawn = false;
+        public string _latitude;
+        public string _longitude;
+
+        public void SetLatitude (string latitude) {
+            _latitude = latitude;
+        }
+
+        public void SetLongitude (string longitude) {
+            _longitude = longitude;
+        }
+
+        public void Spawn () {
+            _location = Conversions.StringToLatLon ($"{_latitude},{_longitude}");
+             _spawnedObject.transform.position = Conversions.GeoToWorldPosition(_location, _map.CenterMercator).ToVector3xz();
+            // _spawnedObject.transform.position = _map.GeoToWorldPosition (_location, true);
+            // _spawnedObject.transform.rotation = averageHeadingAlignmentStrategy._targetRotation;
+            _spawnedObject.SetActive (true);
+            _isSpawn = true;
+        }
+
+        // private void Update () {
+        //     if (!_isSpawn) {
+        //         return;
+        //     }
+        //     _spawnedObject.transform.localPosition = _map.GeoToWorldPosition (_location, true);
+        // }
+    }
+}
